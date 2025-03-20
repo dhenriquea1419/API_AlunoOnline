5cdaadd6e023d27669b0c87ce99135835ab10d14
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-03-20 17:17:29 -0300

    Docker compose e Aplication popertis

--- src/main/java/br/com/alunoonline/api_monitoria/model/Aluno.java
+++ src/main/java/br/com/alunoonline/api_monitoria/model/Aluno.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.IdGeneratorType;
 import org.hibernate.annotations.ValueGenerationType;
 
 @Entity
-@Table
+@Table(name = "aluno")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,11 +16,15 @@ public class Aluno {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id-aluno")
     private Long idAluno;
 
+    @Column(name = "nome-aluno")
     private String nomeAluno;
 
+    @Column(name = "email-aluno")
     private String emailAluno;
 
+    @Column(name = "cpf-aluno")
     private String cpfAluno;
 }
