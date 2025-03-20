aa8c485239825fc3a017f219fbea23b6d3b3792f
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-03-20 14:44:28 -0300

    Criação de Aluno model, aluno repository e aluno controller

--- src/main/java/br/com/alunoonline/api_monitoria/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api_monitoria/controller/AlunoController.java
@@ -1,4 +1,21 @@
 package br.com.alunoonline.api_monitoria.controller;
 
+import br.com.alunoonline.api_monitoria.model.Aluno;
+import br.com.alunoonline.api_monitoria.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createAluno(@RequestBody Aluno aluno){
+        alunoService.createAluno(aluno);
+    }
 }
--- src/main/java/br/com/alunoonline/api_monitoria/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api_monitoria/service/AlunoService.java
@@ -1,4 +1,18 @@
 package br.com.alunoonline.api_monitoria.service;
 
+import br.com.alunoonline.api_monitoria.model.Aluno;
+import br.com.alunoonline.api_monitoria.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlunoService {
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    public void createAluno(Aluno aluno){
+        alunoRepository.save(aluno);
+    }
+
 }
