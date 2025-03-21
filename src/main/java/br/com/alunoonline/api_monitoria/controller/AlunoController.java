73fb85fab0b93d08f09984af27be20ba1034f296
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-03-20 22:32:04 -0300

    Finalização do Crud Aluno

--- src/main/java/br/com/alunoonline/api_monitoria/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api_monitoria/controller/AlunoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -17,5 +20,17 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createAluno(@RequestBody Aluno aluno){
         alunoService.createAluno(aluno);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listAllAlunos(){
+        return alunoService.listAllAlunos();
+    }
+
+    @GetMapping("/{idAluno}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> searchAlunoById(@PathVariable Long idAluno){
+        return alunoService.searchAlunoById(idAluno);
     }
 }
--- src/main/java/br/com/alunoonline/api_monitoria/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api_monitoria/service/AlunoService.java
@@ -3,7 +3,12 @@ package br.com.alunoonline.api_monitoria.service;
 import br.com.alunoonline.api_monitoria.model.Aluno;
 import br.com.alunoonline.api_monitoria.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -13,6 +18,34 @@ public class AlunoService {
 
     public void createAluno(Aluno aluno){
         alunoRepository.save(aluno);
+    }
+
+    public List<Aluno> listAllAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> searchAlunoById(Long idAluno){
+        return alunoRepository.findById(idAluno);
+    }
+
+    public void deleteAlunoById(Long idAluno, Aluno aluno){
+        alunoRepository.deleteById(idAluno);
+    }
+
+    public void updateAlunoById(Long idAluno, Aluno aluno){
+        Optional<Aluno> alunoDataBase = searchAlunoById(idAluno);
+
+        if (alunoDataBase.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado do banco de dados");
+        }
+
+        Aluno alunoEdit = alunoDataBase.get();
+
+        alunoEdit.setNomeAluno(aluno.getNomeAluno());
+        alunoEdit.setCpfAluno(aluno.getCpfAluno());
+        alunoEdit.setEmailAluno(aluno.getEmailAluno());
+
+        alunoRepository.save(alunoEdit);
     }
 
 }
